9f3da3b1088f51b72bc6228a217c482c3f005427
Luis Manuel Palomo Sivianes <luismanuel.palomo@altran.es>
2017-08-22 12:42:51 +0200

    Cherrypick bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { Router, Event, NavigationEnd } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
 
 const LANGUAGE_CATALAN = 'ca';
 
@@ -12,27 +12,31 @@ const LANGUAGE_CATALAN = 'ca';
 export class AppComponent {
 
   constructor(
-    private translateService: TranslateService,
+    private _translateService: TranslateService,
     private router: Router
   ) {
     // The deault language will be Català. This language will be used as
     // a fallback when a translation isn't found in the current language
-    translateService.setDefaultLang(LANGUAGE_CATALAN);
+    _translateService.setDefaultLang(LANGUAGE_CATALAN);
 
     // Lang to use initially
-    translateService.use(LANGUAGE_CATALAN);
+    _translateService.use(LANGUAGE_CATALAN);
 
     // Request for the proper translations everytime a navigation ends
     this.router.events.subscribe((event: Event) => {
       if (event instanceof NavigationEnd) {
-        this.translateService.use(this.translateService.currentLang.split('.')[0] + '.' + event.url.split('/')[1]);
+        this._translateService.use(this._translateService.currentLang.split('.')[0] + '.' + event.urlAfterRedirects.split('/')[1]);
       }
     });
   }
 
   changeLanguage(lang: string): void {
     // First load the common translations and then, the view translations
-    this.translateService.use(lang);
-    this.translateService.use(lang + '.' + this.router.url.split('/')[1]);
+    this._translateService.use(lang);
+    this._translateService.use(lang + '.' + this.router.url.split('/')[1]);
+  }
+
+  get translateService() {
+    return this._translateService;
   }
 }
